28b59dc5459cc5f7912f6c3b89f49df2ff90078d
Sudheesh Katkam <sudheesh@apache.org>
2017-01-25 19:06:51 -0800

    DRILL-4280: CORE (unit tests)
    + Add SimpleMechanism, etc. for testing; see TestSaslExchange
    + Modify TestCustomUserAuthenticator to use PlainMechanism
    + Add TestKerberosSaslAuthentication using Apache Kerby library

---
+++ exec/java-exec/src/test/java/org/apache/drill/exec/rpc/security/SimpleFactory.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.drill.exec.rpc.security;
+
+import org.apache.hadoop.security.UserGroupInformation;
+
+import javax.security.sasl.SaslClient;
+import javax.security.sasl.SaslException;
+import javax.security.sasl.SaslServer;
+import java.io.IOException;
+import java.util.Map;
+
+public class SimpleFactory implements AuthenticatorFactory {
+  private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SimpleFactory.class);
+
+  public static final String MECHANISM_NAME = "SIMPLE";
+
+  @Override
+  public String getSimpleName() {
+    return MECHANISM_NAME;
+  }
+
+  @Override
+  public UserGroupInformation createAndLoginUser(Map<String, ?> properties) throws IOException {
+    return null;
+  }
+
+  @Override
+  public SaslServer createSaslServer(UserGroupInformation ugi, Map<String, ?> properties) throws SaslException {
+    return null;
+  }
+
+  @Override
+  public SaslClient createSaslClient(UserGroupInformation ugi, Map<String, ?> properties) throws SaslException {
+    return null;
+  }
+
+  @Override
+  public void close() {
+    // no op
+  }
+}
