6ca4ef389c8b33a462d0beff74b3076c002823f1
karthik <kmanivannan@maprtech.com>
2017-07-13 17:47:52 -0700

    Addressed PR comments

--- exec/java-exec/src/main/java/org/apache/drill/exec/coord/zk/ZKSecureACLProvider.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/coord/zk/ZKSecureACLProvider.java
@@ -26,22 +26,31 @@ import org.apache.zookeeper.data.ACL;
 import java.util.List;
 
 /**
- * ZKSecureACLProvider restricts access to znodes created by Drill
+ * ZKSecureACLProvider restricts access to znodes created by Drill in a secure installation.
+ *
  * The cluster discovery znode i.e. the znode containing the list of Drillbits is
  * readable by anyone.
- * For all other znodes only the creator of the znode, i.e the Drillbit user, has full access.
+ *
+ * For all other znodes, only the creator of the znode, i.e the Drillbit user, has full access.
+ *
  */
 
 public class ZKSecureACLProvider implements ACLProvider {
 
     static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ZKSecureACLProvider.class);
-    // Creator has full access
+
+    /**
+     * DEFAULT_ACL gives the creator of a znode full-access to it
+     */
     static ImmutableList<ACL> DEFAULT_ACL = new ImmutableList.Builder<ACL>()
                                               .addAll(Ids.CREATOR_ALL_ACL.iterator())
                                               .build();
-    // Creator has full access
-    // Everyone else has only read access
-    static ImmutableList<ACL> DRILL_CLUSTER_ACL = new ImmutableList.Builder<ACL>()
+    /**
+     * DRILL_CLUSTER_ACL gives the creator full access and everyone else only read access.
+     * Used on the Drillbit discovery znode (znode path /<drill.exec.zk.root>/<drill.exec.cluster-id>)
+     * i.e. the node that contains the list of Drillbits in the cluster.
+     */
+     static ImmutableList<ACL> DRILL_CLUSTER_ACL = new ImmutableList.Builder<ACL>()
                                                 .addAll(Ids.READ_ACL_UNSAFE.iterator())
                                                 .addAll(Ids.CREATOR_ALL_ACL.iterator())
                                                 .build();
